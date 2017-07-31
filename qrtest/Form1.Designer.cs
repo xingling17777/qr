918f4192b14c8f34e2d10a391b91fb4331329f0f
xingling17777 <xingling177777@live.cn>
2017-07-31 15:55:06 +0800

    origin

---
+++ qrtest/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace qrtest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 12);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label2.AutoEllipsis = true;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(0, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 97);
+            this.label2.TabIndex = 4;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label3.Font = new System.Drawing.Font("宋体", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(4, 372);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(846, 153);
+            this.label3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(12, 349);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 56);
+            this.label4.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(639, 372);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 110);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "保存当前工作";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(870, 21);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(447, 372);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 110);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "筛选未使用数据";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(870, 525);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ qrtest/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+namespace qrtest
+{
+    
+
+    // 调用  
+   
+    public partial class Form1 : Form
+    {
+        ArrayList customer = new ArrayList();
+        ArrayList current = new ArrayList();
+        ArrayList localScan = new ArrayList();
+       static int n = 0;
+        static int scancount = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //系统显示前加载客户数据
+            
+            if (File.Exists(Directory.GetCurrentDirectory()+"\\customer.txt"))
+            {
+                FileStream fs = new FileStream(Directory.GetCurrentDirectory() + "\\customer.txt", FileMode.Open);
+                StreamReader sr = new StreamReader(fs);
+                string mod;
+                while ((mod = sr.ReadLine()) != null)
+                {
+                    customer.Add(mod.Trim());
+                }
+                sr.Close();
+                fs.Close();
+                label1.Text = "已加载" + customer.Count + "条客户数据";
+            }
+            //加载上次用户数据
+            if (File.Exists(Directory.GetCurrentDirectory() + "\\current.txt"))
+            {
+                FileStream fs = new FileStream("current.txt", FileMode.Open);
+                StreamReader sr = new StreamReader(fs);
+                string mod;
+                while ((mod = sr.ReadLine()) != null)
+                {
+                    current.Add(mod.Trim());
+                }
+                scancount = current.Count;
+                sr.Close();
+                fs.Close();
+                //label1.Text = "已加载" + customer.Count + "条数据";
+            }
+        }
+
+       
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13) { 
+
+            if (textBox1.Text.Trim().ToString() != "")
+            {
+                    string sy = textBox1.Text.Trim().ToString();
+
+
+                    label4.Text = "";
+                    label2.Text = "";
+                    label3.Text = "";
+                    textBox1.Text = "";
+                    if (current.IndexOf(sy) != -1)
+                    {
+                        
+                        if(localScan.IndexOf(sy)!=-1)
+                        {
+label4.Text = "当前重复的二维码为：" + sy+"";
+                            BeepUp.Beep(500, 2000);
+                        MessageBox.Show("当前箱内重复的软管，请继续！"); 
+                        }
+                        else
+                        {
+                            label4.Text = "当前重复的数据为：" + sy + "";
+                            BeepUp.Beep(500, 2000);
+                            MessageBox.Show("非当前箱内重复的软管，请放置等待处理！");
+                        }
+                                               
+                        return;
+                    }
+
+
+                   
+                    if (customer.IndexOf(sy) == -1)
+                    {
+                        label3.Text = "未在客户提供的数据内：" + sy;
+                        BeepUp.Beep(500, 2000);
+                        MessageBox.Show("非法数据，未在客户提供的数据内！");
+                        return;
+                    }
+                    current.Add(sy);
+                    localScan.Add(sy);
+                    label2.Text = "当前已扫描" + (n+1) + "支软管！\n累计扫描"+((n+scancount+1)/447)+"箱"+ ((n + scancount + 1) % 447) + "软管!";                
+                    n++;
+                   if(current.Count % 447==0)
+                    {
+                        FileStream fs = new FileStream(Directory.GetCurrentDirectory() + "\\current.txt", FileMode.Create);
+                        StreamWriter sw = new StreamWriter(fs);
+                        foreach (string s in current)
+                        {
+                            sw.WriteLine(s);
+                        }
+                        sw.Close();
+                        fs.Close();
+
+                        string nameby = DateTime.Now.ToString().Replace(":", "").Replace("/", "").Replace("-", "").Replace(" ", "");
+                        FileStream fk = new FileStream(Directory.GetCurrentDirectory() + "\\" + nameby + ".txt", FileMode.Create);
+                        StreamWriter sk = new StreamWriter(fk);
+                        foreach (string s in localScan)
+                        {
+                            sk.WriteLine(s);
+                        }
+                        localScan.Clear();
+                        sk.Close();
+                        fk.Close();
+                        localScan.Clear();
+                    }
+                
+            }
+        }
+    }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream(Directory.GetCurrentDirectory() + "\\current.txt",FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+            foreach(string s in current)
+            {
+                sw.WriteLine(s);
+            }
+            sw.Close();
+            fs.Close();
+            if(localScan.Count!=0)
+            {
+                string nameby = DateTime.Now.ToString().Replace(":", "").Replace("/","").Replace("-","").Replace(" ","");
+                FileStream fk = new FileStream(Directory.GetCurrentDirectory() + "\\" + nameby+ ".txt", FileMode.Create);
+                StreamWriter sk = new StreamWriter(fk);
+                foreach (string s in localScan)
+                {
+                    sk.WriteLine(s);
+                }
+                localScan.Clear();
+                sk.Close();
+                fk.Close();
+            }
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ArrayList tempSum = new ArrayList();
+            foreach(string s in customer)
+            {
+                if(current.IndexOf(s)==-1)
+                {
+                    tempSum.Add(s);
+                }
+            }
+            FileStream fs = new FileStream(Directory.GetCurrentDirectory() + "\\sum.txt", FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+            foreach (string s in tempSum)
+            {
+                sw.WriteLine(s);
+            }
+            sw.Close();
+            fs.Close();
+        }
+    }
+    public class BeepUp  //新建一个类
+    {
+        /// <param name="iFrequency">声音频率（从37Hz到32767Hz）。在windows95中忽略</param>  
+        /// <param name="iDuration">声音的持续时间，以毫秒为单位。</param>  
+        [DllImport("Kernel32.dll")] //引入命名空间 using System.Runtime.InteropServices;  
+        public static extern bool Beep(int frequency, int duration);
+    }
+}
